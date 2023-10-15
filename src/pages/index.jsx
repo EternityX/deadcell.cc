593e52be47a6607dbdf6378071f6d9c93330a985
eternity <jeramie@protonmail.com>
2023-10-15 06:25:04 -0700

    Slight fix for firefox

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,13 +10,15 @@ export default function Home() {
         <title>DEADCELL</title>
         <meta name="description" content="" />
       </Head>
-      <a href="https://github.com/EternityX">
-      <header className="relative flex items-center justify-center h-screen overflow-hidden">
-        <video className="absolute w-auto min-w-full min-h-full max-w-none" autoPlay loop id="video" onMouseEnter={() => setHovering(true)} onMouseLeave={() => setHovering(false)} muted={!hovering}>
+      
+      <a href="https://github.com/EternityX" className="relative flex items-center justify-center h-screen overflow-hidden">
+        <video className="absolute w-auto min-w-full min-h-full max-w-none" autoPlay loop id="video" onMouseClick={() => setHovering(true)} onMouseEnter={() => setHovering(true)} onMouseLeave={() => setHovering(false)} muted={!hovering}>
           <source src="/video/video.mp4" type="video/mp4"/>
+          
         </video>
-      </header>
+        
       </a>
+      
     </>
   )
 }
