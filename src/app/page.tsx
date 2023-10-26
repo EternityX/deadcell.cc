7872d6bebd13286c5a0e9688ebaf63f67f296b48
eternity <jeramie@protonmail.com>
2023-10-26 06:53:54 -0700

    maybe fix, idk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -94,8 +94,10 @@ export default function Home() {
     setHoveredIcon(null);
   };
 
-  const videos = ['tinkle.mp4', 'aiko.mp4', 'desolate.mp4', 'verge.mp4', 'limbo.mp4', 'laptop.mp4', 'god.mp4', 'smokey.mp4'];
-  const videoToPlay = videos[Math.floor(Math.random() * videos.length)];
+  const getVideoToPlay = () => {
+    const videos = ['tinkle.mp4', 'aiko.mp4', 'desolate.mp4', 'verge.mp4', 'limbo.mp4', 'laptop.mp4', 'god.mp4', 'smokey.mp4'];
+    return videos[Math.floor(Math.random() * videos.length)];
+  }
 
   // @ts-ignore
   return (
@@ -251,7 +253,7 @@ export default function Home() {
             }}
             className={clsx('h-full w-full object-cover')}
           >
-            <source src={'/video/' + videoToPlay} type="video/mp4" />
+            <source src={'/video/' + getVideoToPlay()} type="video/mp4" />
           </video>
         </div>
       </div>
